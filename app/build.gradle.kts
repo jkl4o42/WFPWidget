593b8b3a798cec1aaf6e9d14c2e3b429d0a9c4f0
jkl4o42 <ruslan.savka4o4@gmail.com>
2023-09-24 22:49:20 +0300

    Initial commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,4 +44,5 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.test.ext.junit)
     androidTestImplementation(libs.espresso.core)
+    implementation(project(":widget"))
 }
