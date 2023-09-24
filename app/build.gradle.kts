ccfe7230bc85d0523ebfd00a73a0e0a008c0b68b
jkl4o42 <ruslan.savka4o4@gmail.com>
2023-09-24 21:46:21 +0300

    init

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,7 @@
-@Suppress("DSL_SCOPE_VIOLATION") // TODO: Remove once KTIJ-19369 is fixed
 plugins {
     alias(libs.plugins.androidApplication)
     alias(libs.plugins.kotlinAndroid)
+    id("maven-publish")
 }
 
 android {
@@ -45,4 +45,16 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.test.ext.junit)
     androidTestImplementation(libs.espresso.core)
+}
+
+afterEvaluate {
+    publishing {
+        publications {
+            create<MavenPublication>("maven") {
+                groupId = "com.jkl4o4"
+                artifactId = "wayforpay.widget"
+                version = "0.0.1"
+            }
+        }
+    }
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -15,4 +15,3 @@ dependencyResolutionManagement {
 
 rootProject.name = "WFPWidget"
 include(":app")
- 
